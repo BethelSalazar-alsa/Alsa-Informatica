7d05b859aa99cb886f2d0a18e690dc75605de85c
Bethel-Alsa-Desarrollos <bethel.becerra@alsainformatica.com.mx>
2026-07-16 10:32:40 -0600

    Fix: selected fuera del tree, descripción grande, auto-refresh PDF cada 2s

--- cotizaciones_express/static/src/js/cotizacion_preview.js
+++ cotizaciones_express/static/src/js/cotizacion_preview.js
@@ -1,23 +1,47 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
-import { Component, onWillUpdateProps, useState } from "@odoo/owl";
+import { Component, onMounted, onWillDestroy, useState } from "@odoo/owl";
 
 class CotizacionPreview extends Component {
     static template = "cotizaciones_express.PreviewIframe";
 
     setup() {
-        this.state = useState({ src: this._getSrc(this.props) });
-        onWillUpdateProps((nextProps) => {
-            this.state.src = this._getSrc(nextProps);
+        this.state = useState({ src: this._getSrc() });
+        this._timer = null;
+        onMounted(() => {
+            this._timer = setInterval(() => this._refresh(), 2000);
+            this._bindFormEvents();
+        });
+        onWillDestroy(() => {
+            if (this._timer) clearInterval(this._timer);
+            this._unbindFormEvents();
         });
     }
 
-    _getSrc(props) {
-        const resId = props.record?.resId;
+    _getSrc() {
+        const resId = this.props.record?.resId;
         if (resId) {
             return "/cotizacion/preview_html/" + resId;
         }
         return "";
+    }
+
+    _refresh() {
+        const baseSrc = this._getSrc();
+        if (baseSrc) {
+            this.state.src = baseSrc + "?t=" + Date.now();
+        }
+    }
+
+    _bindFormEvents() {
+        this._onFormChange = () => this._refresh();
+        document.addEventListener("change", this._onFormChange, true);
+    }
+
+    _unbindFormEvents() {
+        if (this._onFormChange) {
+            document.removeEventListener("change", this._onFormChange, true);
+        }
     }
 }
 
