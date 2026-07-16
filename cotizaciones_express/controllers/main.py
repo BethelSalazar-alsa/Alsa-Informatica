3987b8694777aef8ab43aa8537fb41c2b2d822b1
Bethel-Alsa-Desarrollos <bethel.becerra@alsainformatica.com.mx>
2026-07-16 14:23:10 -0600

    Fix: restaurar ruta /cotizacion/preview.js + request.render()

--- cotizaciones_express/controllers/main.py
+++ cotizaciones_express/controllers/main.py
@@ -5,7 +5,7 @@ from odoo.http import request
 class CotizacionExpressController(http.Controller):
 
     @http.route('/cotizacion/preview_html/<int:cotizacion_id>', type='http', auth='user', website=False)
-    def preview_cotizacion_html(self, cotizacion_id):
+    def preview_cotizacion_html(self, cotizacion_id, **kwargs):
         if cotizacion_id > 0:
             cotizacion = request.env['cotizacion.express'].browse(cotizacion_id)
             if not cotizacion.exists():
@@ -13,19 +13,22 @@ class CotizacionExpressController(http.Controller):
         else:
             cotizacion = request.env['cotizacion.express']
 
-        html = request.env['ir.qweb']._render('cotizaciones_express.cotizacion_preview_template', {
+        response = request.render('cotizaciones_express.cotizacion_preview_template', {
             'docs': cotizacion,
             'preview_mode': True,
         })
-        return request.make_response(html, headers=[
-            ('Content-Type', 'text/html; charset=utf-8'),
-            ('Cache-Control', 'no-cache, no-store, must-revalidate'),
-            ('Pragma', 'no-cache'),
-            ('Expires', '0'),
-        ])
+        response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+        return response
+
+    @http.route('/cotizacion/preview.js', type='http', auth='public', website=False)
+    def preview_js(self):
+        js = r"""(function(){function r(){try{var e=parent.location.hash.replace(/^#/,"").split("&");for(var t=0;t<e.length;t++){var n=e[t].split("=");if(n[0]==="id"&&decodeURIComponent(n[1])>0)return parseInt(decodeURIComponent(n[1]),10)}return null}catch(e){return null}}function u(t){fetch("/cotizacion/preview_html/"+t+"?t="+(new Date).getTime()).then(function(r){return r.text()}).then(function(t){document.open();document.write(t);document.close()}).catch(function(){})}function t(){var e=r();if(e&&e>0){u(e);setTimeout(t,3e3)}else{setTimeout(t,1500)}}t()})();"""
+        return request.make_response(js, [('Content-Type', 'application/javascript; charset=utf-8')])
 
     @http.route('/cotizacion/image/<string:model>/<int:record_id>/<string:field>', type='http', auth='user', website=False)
-    def get_image(self, model, record_id, field):
+    def get_image(self, model, record_id, field, **kwargs):
         Model = request.env.get(model)
         if not Model:
             return request.not_found(description='Modelo no encontrado')
@@ -40,9 +43,3 @@ class CotizacionExpressController(http.Controller):
         import base64
         raw = base64.b64decode(value)
         return request.make_response(raw, [('Content-Type', 'image/png')])
-
-    @http.route('/cotizacion/preview.js', type='http', auth='public', website=False)
-    def preview_js(self):
-        js = r"""(function(){function r(){try{var e=parent.location.hash.replace(/^#/,"").split("&");for(var t=0;t<e.length;t++){var n=e[t].split("=");if(n[0]==="id"&&decodeURIComponent(n[1])>0)return parseInt(decodeURIComponent(n[1]),10)}return null}catch(e){return null}}function u(t){fetch("/cotizacion/preview_html/"+t+"?t="+(new Date).getTime()).then(function(r){return r.text()}).then(function(t){document.open();document.write(t);document.close()}).catch(function(){})}function t(){var e=r();if(e&&e>0){u(e);setTimeout(t,3e3)}else{setTimeout(t,1500)}}t()})();"""
-        return request.make_response(js, [('Content-Type', 'application/javascript; charset=utf-8')])
-
