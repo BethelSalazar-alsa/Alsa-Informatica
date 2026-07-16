02f9560c81282cd9a01eb362084cc7dac2bfbdd0
Bethel-Alsa-Desarrollos <bethel.becerra@alsainformatica.com.mx>
2026-07-16 11:45:01 -0600

    Fix: image_data_uri() para logo/firma, HTML preview controller, boton descargar PDF

--- cotizaciones_express/controllers/main.py
+++ cotizaciones_express/controllers/main.py
@@ -1,2 +1,14 @@
 from odoo import http
 from odoo.http import request
+
+
+class CotizacionExpressController(http.Controller):
+
+    @http.route('/cotizacion/preview_html/<int:cotizacion_id>', type='http', auth='user', website=False)
+    def preview_cotizacion_html(self, cotizacion_id):
+        cotizacion = request.env['cotizacion.express'].browse(cotizacion_id)
+        if not cotizacion.exists():
+            return request.not_found()
+        return request.render('cotizaciones_express.cotizacion_preview_template', {
+            'docs': cotizacion,
+        })
--- cotizaciones_express/static/src/js/cotizacion_preview.js
+++ cotizaciones_express/static/src/js/cotizacion_preview.js
@@ -2,14 +2,18 @@
 (function () {
     function refreshPreview() {
         var iframe = document.getElementById("cotizacion_preview_iframe");
+        var pdfLink = document.getElementById("cotizacion_pdf_link");
         if (!iframe) return;
         var form = iframe.closest(".o_form_view");
         if (form && form.dataset.recordId) {
-            iframe.src = "/report/pdf/cotizaciones_express.report_cotizacion_express/"
-                + form.dataset.recordId + "?t=" + Date.now();
+            var id = form.dataset.recordId;
+            iframe.src = "/cotizacion/preview_html/" + id + "?t=" + Date.now();
+            if (pdfLink) {
+                pdfLink.href = "/report/pdf/cotizaciones_express.report_cotizacion_express/" + id;
+            }
         }
     }
-    setInterval(refreshPreview, 3000);
+    setInterval(refreshPreview, 2000);
     document.addEventListener("change", refreshPreview, true);
     document.addEventListener("input", refreshPreview, true);
 })();
