91573f09292ace5d7a58da8f3f2984fc5e4e86a5
Bethel-Alsa-Desarrollos <bethel.becerra@alsainformatica.com.mx>
2026-07-16 12:04:33 -0600

    Fix: JS inline en form view en vez de servicio Odoo module

--- cotizaciones_express/__manifest__.py
+++ cotizaciones_express/__manifest__.py
@@ -15,9 +15,7 @@
         'data/mail_template.xml',
     ],
     'assets': {
-        'web.assets_backend': [
-            'cotizaciones_express/static/src/js/cotizacion_preview.js',
-        ],
+        'web.assets_backend': [],
     },
     'installable': True,
     'application': True,
