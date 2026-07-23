3677e2d069f1f02bc63dac754f8306d17635bd27
Bethel-Alsa-Desarrollos <bethel.becerra@alsainformatica.com.mx>
2026-07-23 15:40:41 -0600

    ejemplo cotizacion contpaqi

--- cotizaciones_express/__manifest__.py
+++ cotizaciones_express/__manifest__.py
@@ -29,6 +29,7 @@ Características Principales:
         'views/cotizacion_views.xml',
         'report/cotizacion_preview.xml',
         'report/cotizacion_report.xml',
+        'report/cotizacion_contpaqi_report.xml',
         'data/mail_template.xml',
     ],
     'assets': {
