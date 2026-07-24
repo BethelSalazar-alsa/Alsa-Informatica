cee67c0645de611c735cbf154faecdc3c1e6f470
Bethel-Alsa-Desarrollos <bethel.becerra@alsainformatica.com.mx>
2026-07-24 16:08:05 -0600

    PDF NUEVO

--- cotizaciones_express/__manifest__.py
+++ cotizaciones_express/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Cotizaciones Express',
-    'version': '19.0.1.0.44',
+    'version': '19.0.1.0.45',
     'category': 'Sales/Sales',
     'summary': 'Generación ágil de cotizaciones comerciales con múltiples opciones, previsualización PDF interactiva en vivo e integración de firmas.',
     'description': """
