c8fe069e55571b67ad282272d6685a3ffa2a5dd3
Bethel-Alsa-Desarrollos <bethel.becerra@alsainformatica.com.mx>
2026-08-05 11:33:53 -0600

    Set fixed reply-to email and mark as sent on comment messages

--- cotizaciones_julio/models/cotizacion.py
+++ cotizaciones_julio/models/cotizacion.py
@@ -251,7 +251,6 @@ class CotizacionExpress(models.Model):
 
     def action_send_to_client(self):
         self.ensure_one()
-        self.state = 'sent'
         template = self.env.ref('cotizaciones_express.email_template_cotizacion', raise_if_not_found=False)
         compose_form = self.env.ref('mail.email_compose_message_wizard_form')
         ctx = {
@@ -259,6 +258,7 @@ class CotizacionExpress(models.Model):
             'default_res_ids': self.ids,
             'default_template_id': template.id if template else False,
             'default_composition_mode': 'comment',
+            'default_reply_to': 'julio.hardware@alsainformatica.com.mx',
             'force_email': True,
         }
         return {
@@ -271,6 +271,14 @@ class CotizacionExpress(models.Model):
             'target': 'new',
             'context': ctx,
         }
+
+    def _message_post_after_hook(self, message, msg_dict):
+        res = super()._message_post_after_hook(message, msg_dict)
+        for record in self:
+            if record.state == 'draft' and msg_dict.get('message_type') in ('email', 'comment'):
+                stage = self.env['cotizacion.express.stage'].search([('state_type', '=', 'sent')], limit=1)
+                record.write({'state': 'sent', 'stage_id': stage.id if stage else False})
+        return res
 
     def action_confirm(self):
         self.ensure_one()
